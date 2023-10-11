2a522861546475ac93a1485bf634bfa38b1933eb
Thomas Bavnhøj <thomas.bavnhoj@gmail.com>
2023-10-11 16:08:20 +0200

    setting up

--- easyAlternativeSolution/main.go
+++ easyAlternativeSolution/main.go
@@ -5,7 +5,7 @@ import (
 	"time"
 )
 
-//TODO Try to simulate windowsize and dropping data / reordering.
+// TODO Try to simulate windowsize and dropping data / reordering.
 
 type header struct {
 	syn        bool
@@ -29,16 +29,16 @@ type serverSimulation struct {
 }
 
 func main() {
-	//Ceater Header
+	// Ceater Header
 	head := header{syn: false, ack: 0, seq: 0, windowSize: 0}
-	//Create client
+	// Create client
 	client := clientSimulation{
 		header:         &head,
 		bytes:          10,
 		channelRecieve: make(chan header, 1),
 		channelSend:    make(chan header, 1),
 	}
-	//Create server
+	// Create server
 	server := serverSimulation{
 		header:         &head,
 		expectedSeq:    0,
@@ -46,24 +46,24 @@ func main() {
 		channelSend:    make(chan header, 1),
 	}
 
-	//Go routine for client and server
+	// Go routine for client and server
 	go clientRoutine(&client, server.channelRecieve)
 	go serverRoutine(&server, client.channelRecieve)
 
-	//Remember not to exit main loop until all goroutines are done.
+	// Remember not to exit main loop until all goroutines are done.
 	time.Sleep(time.Second * 10000)
 }
 
 func clientRoutine(client *clientSimulation, serverChannel chan header) {
 	client.header.syn = true
 	client.header.seq = 0
-	//First handshake. Syn flag on, and we chose to start seq at 0
+	// First handshake. Syn flag on, and we chose to start seq at 0
 	fmt.Println("Client sent first handshake, syn: ", client.header.syn, " and seq:", client.header.seq)
 	serverChannel <- *client.header
-	//Simulate we want to send X bytes (move to param)
+	// Simulate we want to send X bytes (move to param)
 	client.bytes = 20
 	client.header.windowSize = 1000
-	//Third handshake, and now connection is established and data can be sent.
+	// Third handshake, and now connection is established and data can be sent.
 	thirdHandshake := <-client.channelRecieve
 	fmt.Println("Third handshake, server established connection, with Acknowledge", thirdHandshake.ack)
 	fmt.Println("Client is now sending data of total length:", client.bytes)
@@ -73,11 +73,11 @@ func clientRoutine(client *clientSimulation, serverChannel chan header) {
 	for {
 		select {
 		case msg := <-client.channelRecieve:
-			//Rediger her vores header inden vi sender til serveren.
+			// Rediger her vores header inden vi sender til serveren.
 			client.header.seq = msg.ack
 			fmt.Println("Client fik besked fra server. med ACK: ", msg.ack)
 			if msg.ack == client.bytes {
-				//Close vores connection.
+				// Close vores connection.
 				fmt.Println("Client fik besked fra server at vi er færdige med at sende data.")
 				break
 			}
@@ -89,8 +89,8 @@ func clientRoutine(client *clientSimulation, serverChannel chan header) {
 }
 
 func serverRoutine(server *serverSimulation, clientChannel chan header) {
-	//Wait until host wants to establish connection
-	firstHandshake := <-server.channelRecieve //First handshake from client.
+	// Wait until host wants to establish connection
+	firstHandshake := <-server.channelRecieve // First handshake from client.
 	server.header.syn = firstHandshake.syn
 	server.header.ack = firstHandshake.seq + 1
 	fmt.Println("Server sent second handshake, syn:", server.header.syn, " and ack:", server.header.ack)
@@ -106,5 +106,4 @@ func serverRoutine(server *serverSimulation, clientChannel chan header) {
 			continue
 		}
 	}
-
 }
